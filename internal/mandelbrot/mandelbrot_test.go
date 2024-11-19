34aef82673242d1dc557a1498c977d4b56f03e1d
Travis Minke <tminke@Traviss-MacBook-Pro.local>
2024-11-19 15:27:23 -0700

    Fix test

--- internal/mandelbrot/mandelbrot_test.go
+++ internal/mandelbrot/mandelbrot_test.go
@@ -12,7 +12,7 @@ import (
 func TestNewMandelbrotRGBA(t *testing.T) {
 	img := NewMandelbrotRGBA(config.DefaultConfiguration)
 	assert.NotNil(t, img)
-	assert.Equal(t, image.Rectangle{Min: image.Point{X: 0, Y: 0}, Max: image.Point{X: config.DefaultConfiguration.Image.CanvasWidth, Y: config.DefaultConfiguration.Image.CanvasHeight}}, mandelbrotImage.Bounds())
+	assert.Equal(t, image.Rectangle{Min: image.Point{X: 0, Y: 0}, Max: image.Point{X: config.DefaultConfiguration.Image.CanvasWidth, Y: config.DefaultConfiguration.Image.CanvasHeight}}, img.Bounds())
 	assert.Equal(t, BlackColor, img.At(1920, 720))                  // Cartesian 0,0 = Black
 	assert.Equal(t, color.RGBA{71, 71, 71, 255}, img.At(1270, 650)) // Cartesian Point In Seahorse Valley = Gray
 }
@@ -24,7 +24,7 @@ func TestCalculateMandelbrotPixelColor(t *testing.T) {
 
 func TestCalculateGrayscalePixelColor(t *testing.T) {
 	assert.Equal(t, BlackColor, calculateGrayscalePixelColor(4, 2))
-	assert.Equal(t, color.RGBA{54, 54, 54, 0xff}, calculateGrayscalePixelColor(20, 100))
+	assert.Equal(t, color.RGBA{51, 51, 51, 0xff}, calculateGrayscalePixelColor(20, 100))
 }
 
 func TestMapIntRangeToIntRange(t *testing.T) {
